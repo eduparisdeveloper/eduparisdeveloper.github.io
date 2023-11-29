398069b9186491db7ca16a70cbdfb5b77111d241
eduparisdeveloper <eduparis.developer@gmail.com>
2023-11-29 11:59:27 +0100

    build(): error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { GameComponent } from './views/game/game-component.component';
 
 export const routes: Routes = [
-  { path: 'Game', component: GameComponent },
+  { path: '', component: GameComponent, pathMatch: 'full', },
+  { path: 'game', component: GameComponent },
 ];

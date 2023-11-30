1a11c4a1a9984d936380a61bf7a127884053fe19
eduparisdeveloper <eduparis.developer@gmail.com>
2023-11-30 10:29:00 +0100

    build()

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { GameComponent } from './views/game/game-component.component';
 
 export const routes: Routes = [
-  { path: '', component: GameComponent, pathMatch: 'full', },
+  { path: '', component: GameComponent, pathMatch: 'full' },
   { path: 'game', component: GameComponent },
 ];

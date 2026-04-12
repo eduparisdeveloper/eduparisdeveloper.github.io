de4e30195099509b6499354033e1d2863385e640
eduparisdeveloper <eduparis.developer@gmail.com>
2026-04-12 19:31:19 +0200

    feat: remove route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './views/home/home';
-import { Login } from './views/login/login';
+
 
 export const routes: Routes = [
   {
     path: '/',
     component: Home
   },
-  {
-    path: '/login',
-    component:Login
-  }
+
 ];
